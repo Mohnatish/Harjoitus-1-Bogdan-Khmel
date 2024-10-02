51152e68a95996bfa6e94a6c75f2ffe29a1c9849
Mohnatish <156414913+Mohnatish@users.noreply.github.com>
2024-10-02 10:24:51 +0300

    kukuiy

--- Harjoitus 1 Bogdan Khmel/Form1.cs
+++ Harjoitus 1 Bogdan Khmel/Form1.cs
@@ -14,7 +14,7 @@ namespace Harjoitus_1_Bogdan_Khmel
 
         private void button1_Click(object sender, EventArgs e)
         {
-            OtsikkoLB.Text = "Heippa maailma";
+            OtsikkoLB.Text = "Heipp maailma";
         }
     }
 }
